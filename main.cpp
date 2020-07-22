ffcf25f8b3d0fc27ca1ae12e2a27272fb3057eb7
Andrew <jiganyg@gmail.com>
2020-07-23 01:08:59 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-int main() {
-    string word;
-    cin >> word;
-    cout << endl << word << endl;
+int main()
+{
+    string word;      // 宣告變數word
+    cout << "輸入："; //在終端輸出
+    cin >> word;      //從終端讀取輸入到變數word
+    cout << "你剛剛輸入：" << word << endl;
     return 0;
 }
